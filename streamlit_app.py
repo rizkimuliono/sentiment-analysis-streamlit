51ff9fd9276ebfcc0d81f582ebe55a5fd896445f
Rizki Muliono <rizkimuliono@gmail.com>
2024-06-24 00:14:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def train_model():
         st.error("Nilai label tidak dapat dimapping dengan benar.")
         return None
 
-    X = data['tweet']
+    X = data['Text Tweet']
     y = data['label']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
